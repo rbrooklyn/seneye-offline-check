705ebb1acd73326086a28d0eb3e4740bca283012
ribs85 <ribs85@users.noreply.github.com>
2018-11-14 16:38:51 +0000

    Change file to work with docker or pull in enviroment vars for login details

--- seneye-offline-check.py
+++ seneye-offline-check.py
@@ -2,24 +2,13 @@
 import json, requests, calendar, time
 from pushbullet import Pushbullet
 
-## Settings: Change these:
-## Username and password, obviously:
-username = 'me@myemail.com'
-password = 'myPassword'
-## How many minutes until you want an offline alert:
-## You may want to set this to 1 or something to test the script before setting it to a higher value.
-offlineAlertTime = 240
-## Pushbullet API key
-## THIS IS REQUIRED. DO NOT LEAVE THIS AS IS OTHERWISE NOTHING WILL WORK!
-## Go here: https://www.pushbullet.com/#settings/account (you will need to sign up for a pushbullet account)
-## Scroll down to "Access Tokens" then "Create Access Token" copy and paste the resulting code below,
-## remembering to keep within the quotes.
-## For example (don't use this, it won't work):
-## pbAPI = 'o.1234789gbh31byiu3r79'
-pbAPI = 'WhateverPushbulletGaveMe'
-## Ensure you have the pushbullet app installed on your phone and it's logged in.
-## Stop editing now!
+# User configured parts:
+username = os.environ['SENEYE_USERNAME']
+password = os.environ['SENEYE_PASSWORD']
+offlineAlertTime = os.environ['OFFLINE_ALERT_MINUTES']
+pbAPI = os.environ['PUSHBULLET_API_KEY']
 
+# def which sends pushbullet alert if needed:
 def alertUserToOfflineState(tankName , timeOffline):
     pb = Pushbullet(pbAPI)
     my_channel = pb.channels[0]
@@ -31,7 +20,7 @@ url = ('https://api.seneye.com/v1/devices?user=%s&pwd=%s' % (username , password
 # Grab seneye information
 seneye = requests.get(url)
 
-# Go through each tank and check we have been online within the last two hours:
+# Go through each tank and check we have been online within the last X minutes (X defined by offlineAlertTime):
 for tank in seneye.json():
     url=('https://api.seneye.com/v1/devices/%s?IncludeState=1&user=%s&pwd=%s' % (tank["id"], username, password))
     tankinfo = requests.get(url)
